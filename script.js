13f5b86831b43549b4da77ca76481d0fa05c72ee
GiorgiTurmanidze <fiveanderson44@gmail.com>
2023-05-18 16:31:36 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+
+
+// ======================== დავალება 1 ========================
+
+
+// function operator(x,y) {
+//     var operatorInput = prompt("Enter Operator: ")
+//     if (operatorInput == "+") {
+//         console.log(x + y)
+//     }
+//     if (operatorInput == "-") {
+//         console.log(x - y)
+//     }
+//     if (operatorInput == "*") {
+//         console.log(x * y)
+//     }
+//     if (operatorInput == "/") {
+//         console.log(x / y)
+//     }
+//     else{
+//         if(operatorInput != "+" && operatorInput != "-" && operatorInput != "*" && operatorInput != "/") {
+//             alert("Invalid Operator!")
+//         }
+//     }
+// }
+// operator(3,5);
+
+
+
+// ======================== დავალება 2 ========================
+
+
+
+// function circle(radius) {
+//     const pi = 3.14
+//     var answer = pi * radius ^ 2
+//     console.log(answer)
+// }
+// circle(5);
+
+
+
+// ======================== დავალება 3 ========================
+
+
+// function car(make, model, year) {
+//     this.make = make
+//     this.model = model
+//     this.year = year
+//     console.log("მანქანის მარკა და მოდელი არის: " + make + " " + model, " გამოშვების წელი - " + year)
+// }
+// var showCarInfo = new car("bugatti", "cabriolet", "2002")
+// var showCarInfo1 = new car("ferrari", "f40", "1992")
+// var showCarInfo2 = new car("bmw", "m4", "2021")
+// console.log(showCarInfo, showCarInfo1, showCarInfo2)
